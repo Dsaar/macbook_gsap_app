736256754476b5150863413f2b978315d8a98b43
Dsaar <danieljoshuasaar@gmail.com>
2026-07-22 07:40:50 +0300

    Implement Showcase Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/all";
+import Showcase from './components/Showcase';
 
 
 
@@ -15,6 +16,7 @@ const App = () => {
 			<NavBar/>
 			<Hero/>
 			<ProductViewer/>
+			<Showcase/>
 		</main>
 	)
 }

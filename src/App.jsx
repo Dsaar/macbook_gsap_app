57a00dbec957d005eafe64614f5099030c10d3c3
Dsaar <danieljoshuasaar@gmail.com>
2026-07-22 22:48:37 +0300

    Implement Performance Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/all";
 import Showcase from './components/Showcase';
+import Performance from './components/Performance';
+import Features from './components/Features';
+import Highlights from './components/Highlights';
+import Footer from './components/Footer';
 
 
 
@@ -17,6 +21,10 @@ const App = () => {
 			<Hero/>
 			<ProductViewer/>
 			<Showcase/>
+			<Performance/>
+			<Features/>
+			<Highlights/>
+			<Footer/>
 		</main>
 	)
 }
